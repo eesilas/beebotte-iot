fa976c881d0ae5f8dd266dcde5983641677a135f
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-18 14:09:09 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+microIoT.initDisplay()
+microIoT.microIoT_WIFI("EVI-LINKSYS", "88888888")
+microIoT.microIoT_MQTT(
+"9Vo9OLDRP2mZidOOpU8pBRy7",
+"EEuuaO2qa5XrUSV2nEsqe7CveujRovuN",
+"humidity/humidity",
+microIoT.SERVERS.Global
+)
+basic.forever(function () {
+    microIoT.showUserText(0, "Light" + input.lightLevel())
+    microIoT.microIoT_SendMessage(convertToText(input.lightLevel()), microIoT.TOPIC.topic_0)
+})
